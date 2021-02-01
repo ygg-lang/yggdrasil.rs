0452eb224907cf0b22e49ce4098d013c538e695c
Aster <galaster@foxmail.com>
2021-02-01 11:37:40 +0800

    🌿 Support grammar statement

--- grammar.js
+++ grammar.js
@@ -8,23 +8,48 @@ module.exports = grammar({
     supertypes: $ => [
 
     ],
+    inline: $ => [
+        $._grammar_exts
+    ],
     word: $ => $.Id,
 
     rules: {
         program: $ => "program",
 
         // GrammarStatement
+        GrammarStatement: $ => seq(
+            $.Grammar,
+            field("id", $.Id),
+            optional($._grammar_exts),
+            optional($.Eos)
+        ),
+        _grammar_exts: $ => seq(
+            "{",
+            optional(interleave($.String, ",", true)),
+            "}"
+        ),
         Grammar: $ => "grammar!",
 
         // FragmentStatement
-        FragmentStatement : $ => seq(
+        FragmentStatement: $ => seq(
             $.Fragment,
-            field("id", $.Id)
+            field("id", $.Id),
+            optional($.Eos)
         ),
         Fragment: $ => "fragment!",
 
-
+        String: $ => "String",
 
         Id: $ => /[_\p{XID_Start}][_\p{XID_Continue}]*/,
+        Eos: $ => ";"
     }
 });
+
+function interleave(rule, sep, trailing) {
+    if (trailing) {
+        return seq(rule, repeat(seq(sep, rule)), optional(sep))
+    }
+    else {
+        return seq(rule, repeat(seq(sep, rule)))
+    }
+}
