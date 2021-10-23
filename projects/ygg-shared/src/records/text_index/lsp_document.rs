9699886fc518d44e72f6d97a27b36cc1224cca19
Aster <galaster@foxmail.com>
2021-10-24 00:24:09 +0800

    🔮 Find line column of token

--- projects/ygg-shared/src/records/text_index/lsp_document.rs
+++ projects/ygg-shared/src/records/text_index/lsp_document.rs
@@ -1,120 +1,6 @@
-//! Helpers to convert between LSP representations of text documents and Rust
-//! strings.
-//!
-//! ## Motivation:
-//! LSP uses UTF16-encoded strings while Rust's strings are UTF8-encoded. This
-//! means that text offsets in LSP and in Rust are different:
-//! - LSP offsets are in 16-bit code-units and each character is either 1 or 2 of those,
-//! - Rust strings are indexed in bytes and each character takes from 1 to 4 bytes.
-//!
-//! To ensure that LSP client and server "talk" about the same part of a text
-//! document we need a translation layer.
-//!
-//! ## Structure
-//! There are two traits that define the basic functionality on text documents:
-//! - [`TextMap`] defines operations to convert between byte offsets and [`Pos`]'s
-//!   inside a UTF8-encoded string.
-//! - [`TextAdapter`] defines operations to convert between LSP positions, native
-//!   positions, and derived types.
-//!
-//! The work-horse struct that implements both of these traits is
-//! [`IndexedText`]. It wraps the original text and can act as a replacement for
-//! [`String`] where relevant. The struct is generic in the type of text it
-//! wraps, however, so depending on the use-case it can be either:
-//! - `IndexedText<&str>` when you don't really need an ownership of the original
-//!   text, or
-//! - `IndexedText<Arc<str>>` otherwise.
-//!
-//! ## Example usage
-//!
-//! Below is a an example where the original text is `&'static str`.
-//!
-//! ```rust
-//! use lsp_document::{IndexedText, Pos, TextAdapter, TextMap};
-//! use lsp_types::Position;
-//!
-//! // Character width
-//! // U16:     1111111111111 1111111111 1 11 1 1 111111111 21
-//! // U8:      1111111111111 1222122221 1 13 3 3 111111111 41
-//! // U8 offset
-//! //          0         1       2      3       4          5
-//! //          0123456789012 3468013579 0 12 5 8 123456789 04
-//! let text = "Hello, world!\nКак дела?\r\n做得好\nThis is 💣!";
-//! let text = IndexedText::new(text);
-//! // Examples of using TextMap methods
-//! //
-//! // Pos of 💣 from its offset
-//! assert_eq!(text.offset_to_pos(50).unwrap(), Pos::new(3, 8));
-//! // Raw line range info
-//! assert_eq!(text.line_range(2).unwrap(), Pos::new(2, 0)..Pos::new(2, 10));
-//! // Extracting part of text between two positions
-//! assert_eq!(text.substr(Pos::new(1, 7)..Pos::new(1, 15)).unwrap(), "дела");
-//!
-//! // Example of using TextAdapter methods
-//! //
-//! // Pos of `!` after 💣
-//! assert_eq!(text.lsp_pos_to_pos(&Position::new(3, 10)).unwrap(), Pos::new(3, 12));
-//! assert_eq!(text.pos_to_lsp_pos(&Pos::new(3, 12)).unwrap(), Position::new(3, 10));
-//! ```
+use super::*;
 
 use std::{borrow::Borrow, cmp::Ordering, ops::Range};
-
-/// Native position inside a text document/string. Points to a valid position
-/// **before** the character inside a UTF8-encoded string.
-///
-/// ## Why use [`Pos`] instead of raw `usize` offset
-///
-/// This depends on the use-case. Often raw `usize` or a newtype wrapper around
-/// `usize` is sufficient. However, raw byte offsets are not stable at all when a
-/// text document changes.
-///
-/// Usually, a text document is an input to later stages of the pipelines. Let's
-/// take a simple incremental pipeline:
-/// ```text
-/// text: string ->
-///  symbols: Symbol { ..., start: usize, end: usize } ->
-///  diagnostics: Diag { ..., start: usize, end: usize }
-/// ```
-///
-/// Now, any change to `text` on line N will shift all `start` and `end` offsets,
-/// which will invalidate all symbols and diagnostics following the change and
-/// require recomputation.
-///
-/// However, if `start` and `end` are [`Pos`]es then only the line where the
-/// change was made is affected. Symbols and diagnostic for other lines won't be
-/// invalidated.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct LineColumn {
-    /// 0-indexed line inside the text document.
-    pub line: u32,
-    /// 0-indexed byte offset from the beginning of the.
-    /// The offset is at a valid char boundary.
-    pub column: u32,
-}
-
-impl LineColumn {
-    /// Create a new [`Pos`]. This method shouldn't be required to use most of
-    /// the time!
-    ///
-    /// `line` is 0-indexed, `col` is a 0-indexed byte-offset from the beginning
-    /// of the line to a **valid char position**.
-    pub fn new(line: u32, col: u32) -> Self {
-        Self { line, column: col }
-    }
-}
-
-impl PartialOrd for LineColumn {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for LineColumn {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        let line_cmp = self.line.cmp(&other.line);
-        if line_cmp == Ordering::Equal { self.column.cmp(&other.column) } else { line_cmp }
-    }
-}
 
 /// Native representation of a change that replaces a part of the target text.
 ///
@@ -259,25 +145,22 @@ impl<T: TextMap> TextAdapter for T {
 /// `String`, or `Arc<str>`, depending on whether ownership is needed and if it
 /// needs to be unique or shared.
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct IndexedText<T>
-where
-    T: Borrow<str>,
-{
+pub struct IndexedText {
     /// The original text
-    text: T,
+    text: String,
     /// Range of start-end offsets for all lines in the `text`. [`u32`] should be
     /// enough for upto 4GB files; show me a source file like this!
     line_ranges: Vec<Range<u32>>,
 }
 
-impl<T: Borrow<str>> IndexedText<T> {
-    pub fn new(text: T) -> Self {
+impl IndexedText {
+    pub fn new(text: String) -> Self {
         let mut line_ranges: Vec<Range<u32>> = Vec::new();
 
         let mut line_start: Option<usize> = None;
         let mut last_char: Option<(usize, char)> = None;
 
-        let mut char_iter = text.borrow().char_indices().peekable();
+        let mut char_iter = text.char_indices().peekable();
 
         while let Some((pos, c)) = char_iter.next() {
             if line_start.is_none() {
@@ -299,9 +182,9 @@ impl<T: Borrow<str>> IndexedText<T> {
 
             if is_newline {
                 let start = line_start.expect("line_start should be always initialized");
-                debug_assert!(text.borrow().is_char_boundary(start), "Start is not at char boundary");
+                debug_assert!(text.is_char_boundary(start), "Start is not at char boundary");
                 let end = pos + c.len_utf8();
-                debug_assert!(text.borrow().is_char_boundary(end), "End is not at char boundary");
+                debug_assert!(text.is_char_boundary(end), "End is not at char boundary");
 
                 line_ranges.push(start as u32..end as u32);
                 line_start = None;
@@ -319,7 +202,7 @@ impl<T: Borrow<str>> IndexedText<T> {
         }
 
         // Insert an artificial blank line for an empty string
-        if text.borrow().is_empty() {
+        if text.is_empty() {
             line_ranges.push(0..0);
         }
 
@@ -327,7 +210,7 @@ impl<T: Borrow<str>> IndexedText<T> {
     }
 
     fn offset_to_line(&self, offset: usize) -> Option<u32> {
-        match offset.cmp(&self.text.borrow().len()) {
+        match offset.cmp(&self.text.len()) {
             Ordering::Greater => None,
             Ordering::Equal => Some((self.line_ranges.len().max(2) - 2) as u32),
             Ordering::Less => {
@@ -350,13 +233,13 @@ impl<T: Borrow<str>> IndexedText<T> {
         }
     }
 
-    fn pos_to_offset(&self, pos: &LineColumn) -> Option<usize> {
+    fn position_to_offset(&self, pos: &LineColumn) -> Option<usize> {
         let line_range = self.line_ranges.get(pos.line as usize)?;
         Some(line_range.start as usize + (pos.column as usize))
     }
 }
 
-impl<T: Borrow<str>> TextMap for IndexedText<T> {
+impl TextMap for IndexedText {
     fn text(&self) -> &str {
         self.text.borrow()
     }
@@ -384,14 +267,14 @@ impl<T: Borrow<str>> TextMap for IndexedText<T> {
 }
 
 /// Applies a [`TextChange`] to [`IndexedText`] returning a new text as [`String`].
-pub fn apply_change<S: Borrow<str>>(text: &IndexedText<S>, change: TextChange) -> String {
+pub fn apply_change(text: &IndexedText, change: TextChange) -> String {
     match change.range {
         None => change.patch,
         Some(range) => {
             let orig = text.text();
 
-            let offset_start = text.pos_to_offset(&range.start).unwrap();
-            let offset_end = text.pos_to_offset(&range.end).unwrap();
+            let offset_start = text.position_to_offset(&range.start).unwrap();
+            let offset_end = text.position_to_offset(&range.end).unwrap();
             debug_assert!(offset_start <= offset_end, "Expected start <= end, got {}..{}", offset_start, offset_end);
             debug_assert!(offset_end <= orig.len(), "Expected end <= text.len(), got {} > {}", offset_end, orig.len());
 
--- projects/ygg-shared/src/records/text_index/mod.rs
+++ projects/ygg-shared/src/records/text_index/mod.rs
@@ -1,9 +1,11 @@
-mod cst;
-mod lsp;
-mod lsp_document;
+pub(crate) mod cst;
+pub(crate) mod line_column;
+pub(crate) mod lsp;
+pub(crate) mod lsp_document;
+pub(crate) mod traits;
+
 #[cfg(test)]
 mod test;
-mod traits;
 
 use crate::CSTNode;
 use lsp_document::{IndexedText, TextAdapter, TextMap};
@@ -11,11 +13,44 @@ use std::ops::Range;
 
 /// Cache all newlines
 pub struct TextIndex {
-    inner: IndexedText<String>,
+    inner: IndexedText,
     /// Lines count
     lines: usize,
     /// Characters count
     count: usize,
+}
+
+/// Native position inside a text document/string. Points to a valid position
+/// **before** the character inside a UTF8-encoded string.
+///
+/// ## Why use [`Pos`] instead of raw `usize` offset
+///
+/// This depends on the use-case. Often raw `usize` or a newtype wrapper around
+/// `usize` is sufficient. However, raw byte offsets are not stable at all when a
+/// text document changes.
+///
+/// Usually, a text document is an input to later stages of the pipelines. Let's
+/// take a simple incremental pipeline:
+/// ```text
+/// text: string ->
+///  symbols: Symbol { ..., start: usize, end: usize } ->
+///  diagnostics: Diag { ..., start: usize, end: usize }
+/// ```
+///
+/// Now, any change to `text` on line N will shift all `start` and `end` offsets,
+/// which will invalidate all symbols and diagnostics following the change and
+/// require recomputation.
+///
+/// However, if `start` and `end` are [`Pos`]es then only the line where the
+/// change was made is affected. Symbols and diagnostic for other lines won't be
+/// invalidated.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct LineColumn {
+    /// 0-indexed line inside the text document.
+    pub line: u32,
+    /// 0-indexed byte offset from the beginning of the.
+    /// The offset is at a valid char boundary.
+    pub column: u32,
 }
 
 impl TextIndex {
