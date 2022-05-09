e3e837e693edbc3bdd7a13361855b9ae42bce68b
Aster <galaster@foxmail.com>
2022-05-09 10:08:02 +0000

    🔥 Remove error handler with diagnostic

--- projects/ygg-error/src/error_3rd/for_serde_binary.rs
+++
@@ -1,9 +0,0 @@
-use crate::{RuntimeError, YggdrasilError};
-use diagnostic::DiagnosticLevel;
-use serde_binary::Error;
-
-impl From<Error> for YggdrasilError {
-    fn from(error: Error) -> Self {
-        RuntimeError { message: error.to_string() }.as_error(DiagnosticLevel::Error)
-    }
-}
