642136b693cdc3449ac0b23aa5d5a12c4b11be8b
Aster <galaster@foxmail.com>
2022-05-10 22:47:14 +0000

    🔥 Remove pest codegen

--- projects/ygg-bootstrap/build.rs
+++
@@ -1,8 +0,0 @@
-use peginator::buildscript::Compile;
-use std::{env::current_dir, path::PathBuf};
-
-fn main() {
-    let path = PathBuf::from(current_dir().unwrap());
-    let output = path.join("src/parser/ast.rs");
-    Compile::file("ygg.ebnf").destination(output).format().run().unwrap();
-}
--- projects/ygg-cli/tests/main.rs
+++ projects/ygg-cli/tests/main.rs
@@ -1,4 +1,13 @@
+use peginator::buildscript::Compile;
+use std::{env::current_dir, path::PathBuf};
+
 #[test]
 fn ready() {
     println!("it works!")
+}
+
+fn main() {
+    let path = PathBuf::from(current_dir().unwrap());
+    let output = path.join("src/parser/ast.rs");
+    Compile::file("ygg.ebnf").destination(output).format().run().unwrap();
 }
