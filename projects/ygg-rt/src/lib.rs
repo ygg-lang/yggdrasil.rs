dbec04aafa7f42d66706df8a3882fab16457efec
Aster <galaster@foxmail.com>
2022-05-12 03:21:12 +0000

    🐤 Get child or children by kinds

--- projects/ygg-rt/src/lib.rs
+++ projects/ygg-rt/src/lib.rs
@@ -5,6 +5,7 @@ pub(crate) mod macros;
 pub(crate) mod records;
 pub mod traits;
 pub use self::records::text_index::*;
+pub use crate::records::cst_node::{CSTNode, CSTArena};
 #[cfg(feature = "lsp")]
 pub use lsp_types;
 pub use url::Url;
--- projects/ygg-rt/tests/main.rs
+++ projects/ygg-rt/tests/main.rs
@@ -1,4 +1,38 @@
+use std::ops::Deref;
+use std::rc::Rc;
+
+use yggdrasil_rt::{CSTArena, CSTNode};
+
 #[test]
 fn ready() {
     println!("it, works!")
+}
+
+#[repr(u8)]
+pub enum ValkyrieKind {
+    Program,
+    Statement,
+    Expression,
+    Literal,
+    Identifier,
+    Operator,
+    Keyword,
+    Punctuation,
+    Whitespace,
+    Comment,
+    Unknown,
+}
+
+struct StatementNode {
+    pub(crate) raw: Rc<CSTArena<ValkyrieKind>>,
+    pub(crate) proxy: CSTNode<ValkyrieKind>,
+}
+
+impl StatementNode {
+    pub fn children(&self) -> &[CSTNode<ValkyrieKind>] {
+        &self.proxy.children
+    }
+    pub fn leaves(&self) -> &[CSTNode<ValkyrieKind>] {
+        &self.proxy.children
+    }
 }
