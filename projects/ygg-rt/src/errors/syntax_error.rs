97945b87271555cb50c8946795db148a5a704da9
Aster <aster@vers.site>
2022-07-16 16:08:02 +0000

    🔮 Trying to match single quote string

--- projects/ygg-rt/src/errors/syntax_error.rs
+++ projects/ygg-rt/src/errors/syntax_error.rs
@@ -8,19 +8,18 @@ use std::{
 #[derive(Debug)]
 pub struct SyntaxError {
     message: String,
-    file: PathBuf,
+    file: String,
     span: Range<usize>,
 }
 
 impl Display for SyntaxError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        todo!()
-    }
-}
-
-impl Debug for SyntaxError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        if self.file.is_empty() {
+            write!(f, "{}", self.message)
+        }
+        else {
+            write!(f, "{}:{}: {}", self.file, self.span.start, self.message)
+        }
     }
 }
 
@@ -32,7 +31,7 @@ impl From<SyntaxError> for YError {
 
 impl<'i> From<&'i str> for SyntaxError {
     fn from(value: &'i str) -> Self {
-        Self { message: value.to_string(), file: Default::default(), span: Default::default() }
+        Self::new(value)
     }
 }
 
