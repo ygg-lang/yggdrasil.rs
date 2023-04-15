3d2fe91d0e1aa22e0b78a209dccf03b69b145961
Aster <aster@vers.site>
2023-04-15 03:03:50 +0000

    🔧 Fix case of take option element

---
+++ projects/ygg-parser/src/ast/visitor/tokens.rs
@@ -0,0 +1,7 @@
+use super::*;
+
+impl<'i> Extractor<Token_blockContextAll<'i>> for GrammarRule {
+    fn take_one(node: &Token_blockContextAll<'i>) -> Option<Self> {
+        todo!()
+    }
+}
--- projects/ygg-rt/src/iterators/token_pair/mod.rs
+++ projects/ygg-rt/src/iterators/token_pair/mod.rs
@@ -225,6 +225,14 @@ impl<'i, R: YggdrasilRule> TokenPair<'i, R> {
         Err(YggdrasilError::missing_tag(tag, self.get_span()))
     }
 
+    #[inline]
+    pub fn take_tagged_option<N>(&self, tag: Cow<'static, str>) -> Option<N>
+        where
+            N: YggdrasilNode<Rule = R>,
+    {
+        self.take_tagged_one(tag).ok()
+    }
+
     /// Finds the first pair that has its node or branch tagged with the provided
     /// label. Searches in the flattened [`TokenTree`] iterator.
     ///
